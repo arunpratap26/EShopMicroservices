14053bd4bc6a26b7f00cd251f5756135a0127e42
arunpratap26 <arunpratap26@yahoo.com>
2025-07-23 02:27:32 +0530

    created Ordering.API layer exposing minimal api endpoints with carter and REPR pattern

--- src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -1,6 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using FluentValidation;
-using Ordering.Application.Dtos;
+﻿using FluentValidation;
 
 namespace Ordering.Application.Orders.Commands.UpdateOrder;
 public record UpdateOrderCommand(OrderDto Order)
